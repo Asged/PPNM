a90fef95748910710832bb294c8a098944832bf8
Asged <asgerpetersen@yahoo.dk>
2024-04-03 11:11:57 +0200

    More work on EVD

--- homeworks/EVD/main.cs
+++ homeworks/EVD/main.cs
@@ -1,32 +1,30 @@
 class main{
     static void Main(){
         //Testing with timesJ and JTimes with non random matrices
-        matrix A = new matrix(3,3);
+        matrix A = new matrix(2,2);
         A[0,0] = 1;
         A[0,1] = 2;
-        A[0,2] = 3;
-        A[1,0] = 4;
-        A[1,1] = 5;
-        A[1,2] = 6;
-        A[2,0] = 7;
-        A[2,1] = 8;
-        A[2,2] = 9;
-        A.print();
+        A[1,1] = 4;
+        A[1,0] = 3;
 
+        A.print();
         jacobi.timesJ(A,0,1,System.Math.PI/4);
+        jacobi.Jtimes(A,0,1,System.Math.PI/4);
+        
         A.print();
     }
 }
 
 public static class jacobi{
+
 public static void timesJ(matrix A, int p, int q, double theta){
     double c = System.Math.Cos(theta);
     double s = System.Math.Sin(theta);
     for(int i = 0; i<A.size1; i++){
         double aip = A[i,p];
         double aiq = A[i,q];
-        A[i,p] = c*aip - s*aiq;
-        A[i,q] = s*aip + c*aiq; 
+        A[i,p] = c*aip + s*aiq; //p and q are switched
+        A[i,q] = -s*aip + c*aiq; 
     }
     }
 public static void Jtimes(matrix A, int p, int q, double theta){
@@ -35,7 +33,7 @@ public static void Jtimes(matrix A, int p, int q, double theta){
     for(int j = 0; j<A.size1; j++){
         double apj = A[p,j];
         double aqj = A[q,j];
-        A[p,j] = c*apj + s*aqj;
+        A[p,j] = c*apj + s*aqj; //p and q are switched
         A[q,j] = -s*apj + c*aqj; 
     }
     }
