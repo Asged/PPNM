49d821d7393589b2608fe40f05ceefbe4a9e72dd
Asged <asgerpetersen@yahoo.dk>
2024-05-06 16:00:20 +0200

    'fixed plots exercise'

---
+++ Exercises/plots/main.cs
@@ -0,0 +1,8 @@
+class main{
+	public static int Main(){
+		for(double x=-5; x<=5;x+=1.0/128){
+			System.Console.WriteLine($"{x} {sfuns.erf(x)}");
+		}//error func
+		return 0;
+	}//main method
+}//main class	
