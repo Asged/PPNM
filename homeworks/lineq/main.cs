1f6ea205bb4aa2bacada3b343b1dd634ba324ea5
Asged <asgerpetersen@yahoo.dk>
2024-06-13 13:19:10 +0200

    minor changes

--- homeworks/lineq/main.cs
+++ homeworks/lineq/main.cs
@@ -1,11 +1,11 @@
 class main{
 	static void Main(){
-		matrix identity4 = new matrix(4);
-		for (int i = 0; i<4;i++) identity4[i,i] = 1;
+		matrix identity4 = new matrix(5);
+		for (int i = 0; i<5;i++) identity4[i,i] = 1;
 		//Generates random matrix A
 		//int n = 5;
 		//int m = 4;
-		matrix A = new matrix(5,4);
+		matrix A = new matrix(8,5);
 		var rnd = new System.Random(1);
 		for (int n = 0; n < A.size1;n++){
 			for (int m = 0; m < A.size2; m++){
@@ -21,17 +21,19 @@ class main{
 
 		//Checks Q_transpose * Q = I
 		matrix I = Q.transpose() * Q;
-		I.print("Matrix I");
-		System.Console.WriteLine($"{(I).approx(identity4)}");
+		I.print("Matrix Q_transposed * Q");
+		System.Console.WriteLine($"Does Q_transposed * Q approximate to the identity matrix: {(I).approx(identity4)}");
 
 		matrix QR = Q * R;
 		QR.print("Matrix QR");
-		System.Console.WriteLine($"A=QR: {A.approx(QR)}");
+		System.Console.WriteLine($"Does A approximate to QR: {A.approx(QR)}");
 		matrix identity5 = new matrix(5);
 		for (int i = 0; i<5;i++) identity5[i,i] = 1;
+
+
+		System.Console.WriteLine("#################################################################### \n Testing solve for square matrix\n####################################################################");
 		//Random square matrix and vector
 		matrix squareA = new matrix(5,5);
-		squareA.print("Square A matrix");
 		vector b = new vector(5);
 		for (int i = 0; i<squareA.size1; i++){
 			for (int j = 0; j<squareA.size2; j++){
@@ -48,19 +50,22 @@ class main{
 		(matrix squareQ, matrix squareR) = QRGS.decomp(squareA);
 		squareQ.print("Square Q matrix");
 		squareR.print("Square R matrix");
+		matrix squareQR = squareQ * squareR;
+		squareQR.print("QR");
 
 		//Solve for x
 		vector x = QRGS.solve(squareQ,squareR,b);
+		x.print("QRGS solve");
 		vector Ax = squareA*x;
 		Ax.print("Ax");
-		System.Console.WriteLine($"{Ax.approx(b)}");
+		System.Console.WriteLine($"Does Ax approximate b: {Ax.approx(b)}");
 
 		//Square matrix from before is reused
-		matrix inverseA = QRGS.inverse(squareA);
-		inverseA.print("Inverse of square A");
-		matrix squinvA = squareA*inverseA;
-		squinvA.print("A*A^-1");
-		System.Console.WriteLine($"{(squareA*inverseA).approx(identity5)}");
+		matrix squareB = QRGS.inverse(squareA);
+		squareB.print("Inverse of square A");
+		matrix AB = squareA * squareB;
+		AB.print("A*B");
+		System.Console.WriteLine($"Does AB=I :{AB.approx(identity4)}");
 	}
 }
 
