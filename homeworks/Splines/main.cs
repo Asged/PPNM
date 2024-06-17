ac7c5fb7c2cc95593fa2187ded5b166383cc70f0
Asged <asgerpetersen@yahoo.dk>
2024-06-17 22:55:19 +0200

    splines

--- homeworks/Splines/main.cs
+++ homeworks/Splines/main.cs
@@ -4,15 +4,16 @@ class main{
     }//Main
 }//main
 
-public static class splines{
+public class splines{
     public static double linterp(double[] x, double[] y, double z){
         int i=binsearch(x,z);
-        double dx=x[i+1]-x[i]; 
-        if(!(dx>0)) throw new System.Exception("uups..."); //x is only increasing
+        double dx=x[i+1]-x[i]; if(!(dx>0)) throw new System.Exception("uups...");
         double dy=y[i+1]-y[i];
         return y[i]+dy/dx*(z-x[i]);
     }
-    public static int binsearch(double[] x, double z){/* locates the interval for z by bisection */ 
+
+    public static int binsearch(double[] x, double z)
+	{/* locates the interval for z by bisection */ 
 	    if( z<x[0] || z>x[x.Length-1] ) throw new System.Exception("binsearch: bad z");
 	    int i=0, j=x.Length-1;
 	    while(j-i>1){
@@ -21,6 +22,7 @@ public static class splines{
 		}
 	    return i;
 	}
+
     double linterpInteg(double[] x, double[] y, double z){
         int i=binsearch(x,z);
         double dx=x[i+1]-x[i]; 
@@ -28,4 +30,6 @@ public static class splines{
         double dy=y[i+1]-y[i];
         return y[i]*(z-x[i])+dy/dx*System.Math.Pow(z-x[i], 2)/2;
     }
+
+    
 }//splines
