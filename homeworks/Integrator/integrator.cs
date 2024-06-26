535e64082cace3ff8cf201e04604a697ca025ee6
Asged <asgerpetersen@yahoo.dk>
2024-06-26 17:16:21 +0200

    3points for integration

--- homeworks/Integrator/integrator.cs
+++ homeworks/Integrator/integrator.cs
@@ -1,15 +1,43 @@
 using static System.Math;
 using System;
+
 public static class integrator
 {
-    public static double integrate(Func<double, double> f, double a, double b, double delta = 0.001, double eps = 0.001, double f2 = double.NaN, double f3 = double.NaN){
+    public static double integrate(Func<double, double> f, double a, double b, ref int iterations, double delta = 0.001, double eps = 0.001, double f2 = double.NaN, double f3 = double.NaN)
+    {
+        iterations++; // Increment the iteration count
         double h = b - a;
-        if (double.IsNaN(f2)) { f2 = f(a + 2 * h / 6); f3 = f(a + 4 * h / 6); } // first call, no points to reuse
-        double f1 = f(a + h / 6), f4 = f(a + 5 * h / 6);
+        if (double.IsNaN(f2))
+        {
+            f2 = f(a + 2 * h / 6);
+            f3 = f(a + 4 * h / 6);
+        }
+
+        double f1 = f(a + h / 6);
+        double f4 = f(a + 5 * h / 6);
+
         double Q = (2 * f1 + f2 + f3 + 2 * f4) / 6 * (b - a); // higher order rule
         double q = (f1 + f2 + f3 + f4) / 4 * (b - a); // lower order rule
-        double err =  Abs(Q - q);
-        if (err <= delta + eps * Abs(Q)) return Q;
-        else return integrate(f, a, (a + b) / 2, delta /Sqrt(2), eps, f1, f2) + integrate(f, (a + b) / 2, b, delta /Sqrt(2), eps, f3, f4);
+        double err = Abs(Q - q);
+
+        if (err <= delta + eps * Abs(Q))
+        {
+            return Q;
+        }
+        else
+        {
+            double result = integrate(f, a, (a + b) / 2, ref iterations, delta / Sqrt(2), eps, f1, f2) +
+                            integrate(f, (a + b) / 2, b, ref iterations, delta / Sqrt(2), eps, f3, f4);
+            return result;
+        }
+    }
+
+    public static double clenshawintegrate(Func<double, double> f, double a, double b, ref int iterations, double delta = 0.001, double eps = 0.001, double f2 = double.NaN, double f3 = double.NaN)
+    {
+        iterations = 0; // Initialize the iteration count
+        Func<double, double> clenshawf = theta => f((a + b) / 2 + (b - a) / 2 * Cos(theta)) * Sin(theta) * (b - a) / 2;
+
+        double result = integrate(clenshawf, 0, PI, ref iterations, delta, eps, f2, f3);
+        return result;
     }
 }
--- homeworks/Integrator/main.cs
+++ homeworks/Integrator/main.cs
@@ -10,38 +10,56 @@ public static class main{
         double a = 0;
         double b = 1;
 
-        double res = integrator.integrate(func, a, b);
+        int i = 0;
+        double res = integrator.integrate(func, a, b, ref i);
         Console.WriteLine($"The integral of sqrt(x) from {a} to {b} is: {res}, within accuracy: {Abs(res-2.0/3.0)<=acc && Abs((res - (2.0/3.0)) / (2.0/3.0)) <= eps}");
 
-        //1/sqrt(x)
-        func = x => 1/Sqrt(x);
-        res = integrator.integrate(func, a, b);
-        Console.WriteLine($"The integral of sqrt(x) from {a} to {b} is: {res}, within accuracy: {Abs(res-2)<=acc && Abs((res - (2.0)) / (2.0)) <= eps}");
-
+        
         //4Sqrt(1-x^2)
+        i = 0;
         func = x => 4*Sqrt(1-x*x);
-        res = integrator.integrate(func, a, b);
-        Console.WriteLine($"The integral of sqrt(x) from {a} to {b} is: {res}, within accuracy: {Abs(res-PI)<=acc && Abs((res - (PI)) / (PI)) <= eps}");
-
-        //ln(x)/sqrt(x)
-        func = x => Log(x)/Sqrt(x);
-        res = integrator.integrate(func, a, b);
-        Console.WriteLine($"The integral of sqrt(x) from {a} to {b} is: {res}, within accuracy: {Abs(res-(-4))<=acc && Abs((res - (-4)) / (-4)) <= eps}");
+        res = integrator.integrate(func, a, b, ref i);
+        Console.WriteLine($"The integral of 4Sqrt(1-x^2) from {a} to {b} is: {res}, within accuracy: {Abs(res-PI)<=acc && Abs((res - (PI)) / (PI)) <= eps}");
 
         var errorfile = new System.IO.StreamWriter("Errorfunc.txt", append: true);
         for(double x = -5; x<=5; x+=0.001) errorfile.WriteLine($"{x} {Errorfunc(x)}");
         errorfile.Close();
+
+        //1/sqrt(x)
+        i = 0;
+        func = x => 1/Sqrt(x);
+        res = integrator.integrate(func, a, b, ref i);
+        Console.WriteLine($"The integral of 1/sqrt(x) from {a} to {b} is: {res}, within accuracy: {Abs(res-2)<=acc && Abs((res - (2.0)) / (2.0)) <= eps}, number of evaluations: {i}");
+
+        i = 0;
+        func = x => 1/Sqrt(x);
+        res = integrator.clenshawintegrate(func, a, b, ref i);
+        Console.WriteLine($"The integral of 1/Sqrt(x) using clenshaw from {a} to {b} is: {res}, within accuracy: {Abs(res-(2))<=acc && Abs((res - (2)) / (2)) <= eps}, number of integrand evaluations: {i}");
+        Console.WriteLine("Evaluations from scpiy.integrate.quad: 231");
+
+        //ln(x)/sqrt(x)
+        i = 0;
+        func = x => Log(x)/Sqrt(x);
+        res = integrator.integrate(func, a, b, ref i);
+        Console.WriteLine($"The integral of ln(x)/sqrt(x) from {a} to {b} is: {res}, within accuracy: {Abs(res-(-4))<=acc && Abs((res - (-4)) / (-4)) <= eps}, number of evaluations: {i}");
+
+        i = 0;
+        func = x => Log(x)/Sqrt(x);
+        res = integrator.clenshawintegrate(func, a, b, ref i);
+        Console.WriteLine($"The integral of ln(x)/sqrt(x) using clenshaw from {a} to {b} is: {res}, within accuracy: {Abs(res-(-4))<=acc && Abs((res - (-4)) / (-4)) <= eps}, number of integrand evaluations: {i}");
+        Console.WriteLine("Evaluations from scpiy.integrate.quad: 315");
+        Console.WriteLine("Normal integration routine is slower than python but the clenshaw implementation is faster than python");
     }
 
     public static double Errorfunc(double z, double acc = 0.001, double eps = 0.001)
     {
         if (z < 0) return -Errorfunc(-z);
-
+        int i = 0;
         Func<double, double> f1 = t => Exp(-Pow(t, 2));
-        if (z <= 1 && z <= 1) return 2 / Sqrt(PI) * integrator.integrate(f1, 0, z, acc, eps);
-
+        if (z <= 1 && z <= 1) return 2 / Sqrt(PI) * integrator.integrate(f1, 0, z, ref i, acc, eps);
+        i = 0;
         Func<double, double> f2 = t => Exp(-Pow(z + (1 - t)/t, 2))/t/t;
-        if (z > 1) return 1 - 2 / Sqrt(PI) * integrator.integrate(f2, 0, 1, acc, eps);
+        if (z > 1) return 1 - 2 / Sqrt(PI) * integrator.integrate(f2, 0, 1, ref i, acc, eps);
         else throw new Exception($"The argument for the error function is false. Argument: {z}");
     }
 }
